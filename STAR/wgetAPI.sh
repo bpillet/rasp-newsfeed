0d93a9ea23fb0f129d1cdb3d2876b16279b74096
basile pillet <bobpillet@gmail.com>
2016-03-13 19:24:13 +0100

    branche rasp configurée pour hilbert

--- STAR/wgetAPI.sh
+++ STAR/wgetAPI.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 /usr/bin/wget --no-check-certificate `/bin/cat /home/hilbert/rasp-newsfeed/STAR/API_STAR.txt` --output-document=/home/hilbert/rasp-newsfeed/STAR/star.xml
-/bin/sed -i "2i\<?xml-stylesheet href=\"star.xsl\" type=\"text/xsl\"?>\n" /home/hilbert/rasp-newsfeed/STAR/star.xml
--- Weather/wgetAPI.sh
+++ Weather/wgetAPI.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 /usr/bin/wget --no-check-certificate `/bin/cat API.txt` --output-document=/home/hilbert/rasp-newsfeed/Weather/weather.xml
-/bin/sed -i "2i\<?xml-stylesheet href=\"weather.xsl\" type=\"text/xsl\"?>\n" /home/hilbert/rasp-newsfeed/Weather/weather.xml
