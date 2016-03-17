cd3540c3076acb473bd1418e282161723d1af7e9
basile pillet <bobpillet@gmail.com>
2016-03-17 18:28:39 +0100

    correction cat API.txt

--- Weather/wgetAPI.sh
+++ Weather/wgetAPI.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/wget --no-check-certificate `/bin/cat API.txt` --output-document=/home/hilbert/rasp-newsfeed/Weather/weather.xml
+/usr/bin/wget --no-check-certificate `/bin/cat /home/hilbert/rasp-newsfeed/Weather/API.txt` --output-document=/home/hilbert/rasp-newsfeed/Weather/weather.xml
